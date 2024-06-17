9bfdbb4032f9073a923ca2a1e17b3c36b84c4f80
r.gedhar007@gmail.com <Ravi@#9767@#>
2024-06-17 22:29:53 +0530

    add Shopping project

---
+++ Shopping/js/main.js
@@ -0,0 +1,4 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import "../node_modules/bootstrap/dist/js/bootstrap.esm";
+import "../node_modules/bootstrap/dist/js/";
